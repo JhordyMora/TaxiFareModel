5ddb8007359c2d83585dbf61143031785ea93cf8
jhordymora <jhordy.mora@web.de>
2021-08-17 15:59:46 +0200

    Finally

--- TaxiFareModel/encoders.py
+++ TaxiFareModel/encoders.py
@@ -1,4 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+from TaxiFareModel.utils import haversine_vectorized
 
 
 class TimeFeaturesEncoder(BaseEstimator, TransformerMixin):
@@ -27,16 +29,12 @@ class TimeFeaturesEncoder(BaseEstimator, TransformerMixin):
 
 
 class DistanceTransformer(BaseEstimator, TransformerMixin):
-     """
+    """
         Computes the haversine distance between two GPS points.
         Returns a copy of the DataFrame X with only one column: 'distance'.
     """
-
-    def __init__(self,
-                 start_lat="pickup_latitude",
-                 start_lon="pickup_longitude",
-                 end_lat="dropoff_latitude",
-                 end_lon="dropoff_longitude"):
+    
+    def __init__(self, start_lat="pickup_latitude", start_lon="pickup_longitude", end_lat="dropoff_latitude", end_lon="dropoff_longitude"):
         self.start_lat = start_lat
         self.start_lon = start_lon
         self.end_lat = end_lat
